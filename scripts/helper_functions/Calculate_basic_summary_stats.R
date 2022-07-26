b680fd1b485527725368140ad7eae51431812d99
Leimberger <leimberk@oregonstate.edu>
2022-07-26 13:13:09 -0700

    Tidying scripts that summarize camera effort

---
+++ scripts/helper_functions/Calculate_basic_summary_stats.R
@@ -0,0 +1,20 @@
+#Function to calculate min, max, mean, sd, and median
+#Need to pass arguments with {{}}. See https://cran.r-project.org/web/packages/dplyr/vignettes/programming.html
+calculate_basic_summary_stats <- function(data, variable){
+  
+  sum <- data %>%
+    dplyr::summarise(min = min({{ variable }}, na.rm = TRUE),
+                     max = max({{ variable }}, na.rm = TRUE),
+                     mean = mean({{ variable }}, na.rm = TRUE),
+                     sd = sd({{ variable }},  na.rm = TRUE),
+                     median = median({{ variable }}, na.rm = TRUE))
+  
+  return(sum)
+  
+}
+
+
+
+  
+
+
