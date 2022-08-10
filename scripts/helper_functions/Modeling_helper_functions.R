5e75b827720f029a576988e828d81fa6c70a0f95
Leimberger <leimberk@oregonstate.edu>
2022-08-09 21:09:51 -0700

    Analyzing specialization metrics

--- scripts/helper_functions/Modeling_helper_functions.R
+++ scripts/helper_functions/Modeling_helper_functions.R
@@ -6,10 +6,128 @@
 calculate_ci <- function(model, method_name){
   
   params <- vcov(model)$cond %>% rownames()
-  
   ci <- confint(object = model, parm = params, method = method_name)
   
   return(ci)
   
 }
 
+
+#########################################################
+#Extract information from model object about convergence
+########################################################
+check_convergence <- function(model){
+  
+  convergence <- model$fit$convergence
+  
+  return(convergence) #If 0, it converged
+  
+}
+
+#######################
+#Check random effects
+#######################
+check_re <- function(model, plot_title, plot_subtitle){
+  
+  title = bquote(atop(bold(.(plot_title)), textstyle(.(plot_subtitle))))
+  plot <- cowplot::plot_grid(plotlist = lme4:::dotplot.ranef.mer(ranef(model)$cond)) %>% ggpubr::annotate_figure(., top=text_grob(title)) 
+  
+  return(plot)
+  
+}
+
+###########################################
+#Make and store plots produced from DHARMa
+###########################################
+make_dharma_plot <- function(dharma_object, plot_type, plot_title = NA, plot_subtitle = NA){
+  
+  if(is.na(plot_title)){plot_title = ""}
+  if(is.na(plot_subtitle)){plot_subtitle = ""}
+  title = bquote(atop(bold(.(plot_title)), textstyle(.(plot_subtitle))))
+  
+  if(plot_type == "basic"){plot(dharma_object)}
+  if(plot_type == "overdispersion"){testDispersion(dharma_object)}
+  if(plot_type == "zeroinflation"){testZeroInflation(dharma_object)}
+  if(plot_type == "outlier"){testOutliers(dharma_object)}
+  
+  plot <- grDevices::recordPlot()
+  plot <- cowplot::plot_grid(plot) %>% annotate_figure(., top=text_grob(title)) 
+  plot.new()
+  
+  return(plot)
+  
+}
+
+########################################################################
+#Create residual vs. predicted DHARMa plots for each predictor variable
+########################################################################
+#DHARMa version of plots, created using plotResiduals function
+#'predictor_table' is a table of x-variables, with a column called 'xvar'
+make_dharma_xvar_plot <- function(predictor_table, dataset, dharma_object, plot_title, plot_subtitle) {
+  
+  #Internal function to make plots for each predictor
+  make_plot = function(xvar){
+    
+    xvar <- sym(xvar)
+    xvar_column <- dataset %>% pull(!!xvar) #Pull is equivalent to dollar sign. Returns vector with length > 1
+    plotResiduals(dharma_object$scaledResiduals, as.factor(xvar_column))
+    mtext(paste(xvar), side = 1, line = 4, font = 2)
+    plot <- recordPlot() #Need to use recordPlot because base R plots cannot be stored (return NULL)
+    plot.new()
+    
+    return(plot)
+    
+  }
+  
+  predictors_and_plots = predictor_table %>%
+    mutate(plot = map(xvar, ~make_plot(xvar = .)))
+  
+  title = bquote(atop(bold(.(plot_title)), textstyle(.(plot_subtitle))))
+  multiplot = cowplot::plot_grid(plotlist = predictors_and_plots$plot, scale = 0.8) %>% annotate_figure(., top=text_grob(title)) 
+  
+  return(multiplot)
+}
+
+#######################################
+#Extract information about sample size 
+######################################
+#Total number of observations
+get_number_obs <- function(model){
+  
+  num_obs <- model$modelInfo$nobs
+  
+  return(num_obs)
+}
+
+#Number of levels within each random effect/group
+get_number_re_levels <- function(model){
+  
+  #Get number of random effects
+  num_re <- model$modelInfo$reStruc$condReStruc %>% length()
+  
+  #Get number of levels within each re
+  results <- list() #empty results list
+  for(i in 1:num_re){
+    num_levels <- model$modelInfo$reStruc$condReStruc[[i]]$blockReps 
+    re_name <- paste("group", i, ":", sep = " ")
+    results[[re_name]] <- num_levels #created a named list
+  }
+  
+  return(results)
+}
+
+#Number of replicates/etc. that went into model
+#Default is number of control & treatment replicates
+get_sample_size <- function(data, vars = c("year", "patch", "control_treatment"), grouping_var = "control_treatment"){
+  
+  vars <- syms(vars)
+  grouping_var <- sym(grouping_var)
+  
+  sum <- data %>%
+    distinct(!!!vars) %>%
+    group_by(!!grouping_var) %>%
+    summarise(n = n())
+  
+  return(sum)
+  
+}
---
+++ scripts/helper_functions/Plotting_helper_functions.R
@@ -0,0 +1,107 @@
+#############################
+##Make interaction plot
+############################
+#Plot estimated marginals means for:
+#control pre, control post, treatment pre, treatment post
+make_interaction_plot <- function(ggeffects_df, yvar, ymax, quick_plot = FALSE){
+  
+  #Colors and shapes
+  colors <- c("#0E0D37", "#BA0022") #blue, red
+  shapes <- c(16, 17) #circle, triangle (filled)
+  
+  #Label for y-axis
+  # if(yvar == "H2"){ylabel <- c(expression(atop("Network specialization", paste("("*italic(H[2])*"')"))))}
+  # if(yvar == "d"){ylabel <-  c(expression(atop("Species specialization", paste("(mean "*italic(d)*"')"))))}
+  # if(yvar == "species.specificity.index"){ylabel <- c(expression(atop("Species specialization", paste("(mean SSI)"))))}
+  
+  if(yvar == "H2"){ylabel <- c(expression(atop(paste(italic(H[2])*"'"), "(network)")))}
+  if(yvar == "d"){ylabel <-  c(expression(atop(paste("mean "*italic(d)*"'"), "(species)")))}
+  if(yvar == "species.specificity.index"){ylabel <- c(expression(atop(paste("mean SSI"), "(species)")))}
+  if(yvar == "num_morphotypes"){ylabel <- c("# morphotypes")}
+  
+  #Group/legend labels
+  group_labels <- c("control" = "Control", "treatment" = "Treatment")
+  legend_label <- c(expression(paste(italic("Heliconia "), "removal", sep = "")))
+  
+  #Quick plot from ggeffects package
+  if(quick_plot == TRUE){
+    
+    quick_plot <- plot(ggeffects_df,
+                       connect.lines = TRUE,
+                       colors = colors,
+                       show.title = FALSE) +
+      labs(x = "Experimental phase", y = ylabel, colour = "")
+    
+    return(quick_plot)
+    
+  }
+ 
+  #Plotting by hand for increased customizability
+  plot <- ggeffects_df %>%
+    mutate(x = factor(x, levels = c("pre", "post"), labels = c("Pre", "Post"))) %>%
+    ggplot(data = ., aes(x = x, y = predicted, colour = group, shape = group)) + 
+      geom_point(aes(x = x, y = predicted, colour = group), position = position_dodge(width = 0.25), size = 3.5) +
+      geom_line(aes(group = group), position = position_dodge(0.25), alpha = 0.6, size = 1) +
+      geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(width = 0.25), width = 0.00, size = 1) +
+      theme_bw(base_size = 18) +
+      theme(legend.position = "bottom", legend.direction = "horizontal", panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      scale_y_continuous(labels = scales::number_format(accuracy = 0.1, decimal.mark = '.')) +
+      scale_color_manual(values = colors, labels = group_labels) +
+      scale_shape_manual(values = shapes, labels = group_labels) +
+      labs(x = "Experimental phase", y = ylabel, colour = legend_label, shape = legend_label) +
+      ylim(0, ymax)
+    
+  return(plot)
+  
+}
+
+###########################################################
+#Make plot of interaction contrast + CI ("contrast plot")
+###########################################################
+#Will need to customize axes, etc. separately, but this will make the basic plot
+#estimate_name = name of the column that has the point estimate (e.g., odds.ratio, ratio, etc.)
+make_contrast_plot <- function(contrasts_x2_df, estimate_name, xaxis_name, shading = "none"){
+  
+  estimate_name <- sym(estimate_name)
+  xaxis_name <- sym(xaxis_name)
+  
+  legend_text_labels <- c(expression("All species"), expression(paste(italic("Heliconia "), "specialists", sep = "")))
+  
+  plot_data <- contrasts_x2_df %>%
+    mutate(order = 1:length(!!xaxis_name))
+  
+  if(shading == "below"){
+    
+    plot <- plot_data %>%
+      ggplot(data = ., aes(x = forcats::fct_reorder(!!xaxis_name, order), y = !!estimate_name, shape = bird_groups)) +
+        geom_rect(xmax = Inf, xmin = -Inf, ymax = 1, ymin = 0, fill = "grey90", alpha = 1)
+  }
+  
+  if(shading == "above"){
+    
+    plot <- plot_data %>%
+      ggplot(data = ., aes(x = forcats::fct_reorder(!!xaxis_name, order), y = !!estimate_name, shape = bird_groups)) +
+        geom_rect(xmax = Inf, xmin = -Inf, ymax = Inf, ymin = 1, fill = "grey90", alpha = 1)
+    
+  }
+  
+  if(shading == "none"){
+    
+    plot <- plot_data %>%
+      ggplot(data = ., aes(x = forcats::fct_reorder(!!xaxis_name, order), y = !!estimate_name, shape = bird_groups))
+    
+  }
+  
+  plot <- plot +
+      geom_point(position = position_dodge(.5), size = 3) +
+      geom_errorbar(position = position_dodge(.5), aes(ymax = upperCI, ymin = lowerCI), width = 0.25, size = 1) + # error bars show 95% confidence intervals
+      geom_hline(yintercept = 1, color = "black", linetype = "dashed", alpha = 0.8) + # add a line at 1 (no effect)
+      theme_bw(base_size = 18) +
+      scale_shape_manual(values = c(16, 17), labels = legend_text_labels, limits = c("all_spp", "greh_visa")) +
+      theme(legend.position = "top", legend.justification = "center",
+            panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      labs(x = "", y = "Change ratio", shape = "Bird group")
+    
+  return(plot)
+  
+}
