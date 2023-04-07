5f0808e21f5fd319936d38c48c00774b96bfaddb
Leimberger <kleimberger@gmail.com>
2023-04-07 15:02:36 -0700

    moving analysis code to separate folder and adding bias adjustment in emmeans

--- code/helper_functions/Plotting_helper_functions.R
+++ code/helper_functions/Plotting_helper_functions.R
@@ -3,20 +3,16 @@
 ############################
 #Plot estimated marginals means for:
 #control pre, control post, treatment pre, treatment post
-make_interaction_plot <- function(ggeffects_df, yvar, ymax, sampling_method, add_icon = TRUE){
+make_interaction_plot <- function(emmeans_df, sampling_method, yvar, ymax, add_icon = TRUE){
   
-  camera_icon <-  png::readPNG("../code/camera.png")
-  pollen_icon <-  png::readPNG("../code/pollen.png")
+  camera_icon <-  png::readPNG("code/analysis/camera.png")
+  pollen_icon <-  png::readPNG("code/analysis/pollen.png")
   
   #Colors and shapes
   colors <- c("#0E0D37", "#BA0022") #blue, red
   shapes <- c(16, 17) #circle, triangle (filled)
   
-  #Label for y-axis
-  # if(yvar == "H2"){ylabel <- c(expression(atop("Network specialization", paste("("*italic(H[2])*"')"))))}
-  # if(yvar == "d"){ylabel <-  c(expression(atop("Species specialization", paste("(mean "*italic(d)*"')"))))}
-  # if(yvar == "species.specificity.index"){ylabel <- c(expression(atop("Species specialization", paste("(mean SSI)"))))}
-  
+  #Labels for y-axis
   if(yvar == "H2"){ylabel <- c(expression(paste(italic(H[2])*"′")))}
   if(yvar == "d"){ylabel <-  c(expression(paste("mean "*italic(d)*"′")))}
   if(yvar == "species.specificity.index"){ylabel <- c("mean SSI")}
@@ -27,15 +23,19 @@ make_interaction_plot <- function(ggeffects_df, yvar, ymax, sampling_method, add
   legend_label <- c(expression(paste(italic("Heliconia "), "removal", sep = "")))
  
   #Plotting by hand for increased customization
-  plot <- ggeffects_df %>%
-    mutate(group = factor(group, levels = c("control", "treatment"), labels = c("Control", "Treatment"))) %>%
-    mutate(x = factor(x, levels = c("pre", "post"), labels = c("Pre", "Post"))) %>%
-    ggplot(data = ., aes(x = x, y = predicted, colour = group, shape = group)) + 
-      geom_point(aes(x = x, y = predicted, colour = group), position = position_dodge(width = 0.25), size = 3.5) +
-      geom_line(aes(group = group), position = position_dodge(0.25), alpha = 0.6, size = 1) +
-      geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(width = 0.25), width = 0.00, size = 1) +
+  plot <- emmeans_df %>%
+    mutate(control_treatment = factor(control_treatment, levels = c("control", "treatment"), labels = c("Control", "Treatment"))) %>%
+    mutate(exp_phase = factor(exp_phase, levels = c("pre", "post"), labels = c("Pre", "Post"))) %>%
+    ggplot(data = ., aes(x = exp_phase, y = estimate, colour = control_treatment, shape = control_treatment)) + 
+      geom_point(aes(x = exp_phase, y = estimate, colour = control_treatment), position = position_dodge(width = 0.25), size = 3.5) +
+      geom_line(aes(group = control_treatment), position = position_dodge(0.25), alpha = 0.6, linewidth = 1) +
+      geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(width = 0.25), width = 0.00, linewidth = 1) +
       theme_bw(base_size = 18) +
-      theme(legend.position = "bottom", legend.direction = "horizontal", panel.grid.major = element_blank(), panel.grid.minor = element_blank(), axis.text = element_text(size = 18)) +
+      theme(legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.text = element_text(size = 18),
+            legend.title = element_text(size = 18),
+            panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
       scale_y_continuous(labels = scales::number_format(accuracy = 0.1, decimal.mark = '.')) +
       scale_color_manual(values = colors, labels = group_labels) +
       scale_shape_manual(values = shapes, labels = group_labels) +
@@ -67,27 +67,37 @@ make_interaction_plot <- function(ggeffects_df, yvar, ymax, sampling_method, add
 #Make control vs. treatment plot
 ##################################
 #This plot is similar to interaction plot, but for response variables not calculated at the level of experimental period (control vs. treatment only)
-make_control_vs_treatment_plot <- function(ggeffects_df, yvar, ymax){
+make_control_vs_treatment_plot <- function(emmeans_df, yvar, ymax){
   
-  #Label for y-axis
-  if(yvar == "WN"){ylabel <- c("Total")}
-  if(yvar == "ST"){ylabel <-  c("Species gain or loss")}
-  if(yvar == "OS"){ylabel <- c("Among shared species")}
+  #Labels for y-axis
+  if(yvar == "WN"){ylabel <- c("Total turnover")}
+  if(yvar == "ST"){ylabel <-  c("Species turnover")}
+  if(yvar == "OS"){ylabel <- c("Among-species turnover")}
   
-  plot <- ggeffects_df %>%
-    mutate(x = factor(x, levels = c("control", "treatment"), labels = c("Control", "Treatment"))) %>%
-    ggplot(data = ., aes(x = x, y = predicted)) + 
-    geom_point(aes(x = x, y = predicted), position = position_dodge(width = 0.25), size = 4) +
-    geom_errorbar(aes(ymin = conf.low, ymax = conf.high), position = position_dodge(width = 0.25), width = 0.0, size = 1) +
-    theme_bw(base_size = 18) +
-    theme(legend.position = "none",
-          legend.direction = "horizontal",
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          plot.title = element_text(hjust = 0.5),
-          axis.text = element_text(size = 18)) +
-    labs(x = "", y = ylabel, shape = "", color = "") +
-    ylim(0, ymax)
+  #Colors and shapes
+  colors <- c("#0E0D37", "#BA0022") #blue, red
+  shapes <- c(16, 17) #circle, triangle (filled)
+  
+  #Group/legend labels
+  group_labels <- c("control" = "Control", "treatment" = "Treatment")
+  legend_label <- c(expression(paste(italic("Heliconia "), "removal", sep = "")))
+  
+  plot <- emmeans_df %>%
+    mutate(control_treatment = factor(control_treatment, levels = c("control", "treatment"), labels = c("Control", "Treatment"))) %>%
+    ggplot(data = ., aes(x = control_treatment, y = estimate, colour = control_treatment, shape = control_treatment)) +
+      geom_point(aes(x = control_treatment, y = estimate), position = position_dodge(width = 0.25), size = 4) +
+      geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(width = 0.25), width = 0.0, linewidth = 1) +
+      theme_bw(base_size = 18) +
+      theme(legend.position = "none",
+            legend.direction = "horizontal",
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.title = element_text(hjust = 0.5),
+            axis.text = element_text(size = 18)) +
+      scale_color_manual(values = colors, labels = group_labels) +
+      scale_shape_manual(values = shapes, labels = group_labels) +
+      labs(x = "", y = ylabel, colour = "", shape = "") +
+      ylim(0, ymax)
   
   return(plot)
   
@@ -129,7 +139,7 @@ make_contrast_plot <- function(contrasts_df, xvar, shading = "none"){
   
   plot <- plot +
       geom_point(position = position_dodge(.5), size = 3) +
-      geom_errorbar(position = position_dodge(.5), aes(ymax = upper.CL, ymin = lower.CL), width = 0.25, size = 1) + # error bars show 95% confidence intervals
+      geom_errorbar(position = position_dodge(.5), aes(ymax = upper.CL, ymin = lower.CL), width = 0.25, linewidth = 1) + # error bars show 95% confidence intervals
       geom_hline(yintercept = 1, color = "black", linetype = "dashed", alpha = 0.8) + # add a line at 1 (no effect)
       theme_bw(base_size = 18) +
       scale_shape_manual(values = c(16, 17), labels = legend_text_labels, limits = c("all_spp", "greh_visa")) +
